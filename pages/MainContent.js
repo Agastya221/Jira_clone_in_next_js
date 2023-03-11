d963e7d5b48c41cee80b0cc8f9527225c0bcb79f
agastya221 <agastyash213@gmail.com>
2023-03-11 13:32:54 +0530

    Few Edit in MainContent

--- pages/MainContent.js
+++ pages/MainContent.js
@@ -1,10 +1,29 @@
 import { Container } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
+import css from '../styles/Main.module.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
 
 const mainContent = () => {
   return (
     <Container className='Main'>
-      
+      <div className={css.rootfileDesign}>Projects
+      <span className={css.line}>/
+      </span>singularity 1.0
+      <span className={css.line}>/</span>Kanban Board
+      </div>
+      <div className={css.Header}>
+        <div className={css.Kanban}>Kanban Board</div>
+        <div className={css.linkheading}>
+        <Link className={css.links} href={"https://github.com/Agastya221/Jira_clone_in_next_js"}>
+          <i className={css.icon}><GitHubIcon fontSize='small'/></i>
+        </Link>
+        <p className={css.p}>Github Repo</p>
+        </div>
+        
+      </div>
     </Container>
   )
 }

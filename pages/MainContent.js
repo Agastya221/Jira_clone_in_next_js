5607c2f8960008d0bb29bc38a0e72a7ee5a8aeb3
agastya221 <agastyash213@gmail.com>
2023-05-27 13:04:14 +0530

    updated the DND to work better

--- pages/MainContent.js
+++ pages/MainContent.js
@@ -1,7 +1,7 @@
 import { Container } from '@mui/material'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Link from 'next/link'
-import React from 'react'
+import React,{useEffect,useState, useId} from 'react'
 import css from '../styles/Main.module.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
@@ -9,112 +9,212 @@ import Sidenavbar from './Sidenavbar';
 import Navbar from './Navbar';
 import { FiSearch } from "react-icons/fi";
 
+const MainContent = () => {
+  const [isBrowser, setIsBrowser] = useState(false);
+  const data = [
+    {
+      id: useId(),
+      text: "Try leaving a comment on this issue.",
+    },
+    {
+      id: useId(),
+      text: "this is just for testing"
+    },
+    {
+      id: useId(),
+      text: "this is just for "
+    },
+   
+  ];
+ 
+  const data1 = [
+    {
+      id: useId(),
+      text: "Try leaving a comment on this issue. ahgdvajdv",
+    },
+    {
+      id: useId(),
+      text: "this is just for testing asdasd"
+    },
+    {
+      id: useId(),
+      text: "this is just for  asdad"
+    },
+   
+  ];
+  const data2 = [
+    {
+      id: useId(),
+      text: "Try leaving a comment on this issue. ahgdvajdv adsfljjsDkjfsDKfhkadshfuk",
+    },
+    {
+      id: useId(),
+      text: "this is just for testing asdasd"
+    },
+    {
+      id: useId(),
+      text: "this is just for  asdad"
+    },
+   
+  ];
+ 
+const dataWithIndexes = data.map((item, index) => {
+  return {
+    ...item,
+    index
+  };
+});
+const dataWithIndexes1 = data1.map((item, index) => {
+  return {
+    ...item,
+    index
+  };
+});
+const dataWithIndexes2 = data2.map((item, index) => {
+  return {
+    ...item,
+    index
+  };
+});
+
+useEffect(() => {
+  setIsBrowser(process.browser);
+}, [])
 
 
-const mainContent = () => {
-  return (<>
-    <Sidenavbar />
-    <Navbar />
-    <Container className='Main'>
-
-      <div className={css.rootfileDesign}>Projects
-        <span className={css.line}>/
-        </span>singularity 1.0
-        <span className={css.line}>/</span>Kanban Board
-      </div>
-      <div className={css.Header}>
-        <div className={css.Kanban}>Kanban board</div>
-        <div className={css.linkheading}>
-          <Link className={css.links} href={"https://github.com/Agastya221/Jira_clone_in_next_js"}>
-            <i className={css.icon}><GitHubIcon fontSize='small' /></i>
-            <p className={css.p}>Github Repo</p>
-          </Link>
-
+  return (
+    <>
+      <Sidenavbar />
+      <Navbar />
+      <Container className='Main'>
+        <div className={css.rootfileDesign}>Projects
+          <span className={css.line}>/
+          </span>singularity 1.0
+          <span className={css.line}>/</span>Kanban Board
         </div>
-      </div>
-      <div className={css.container} >
-        <div className={css.inputContainer}>
-          <i className={css.search}><FiSearch color='black' /></i>
-          <input className={css.input} type="text" />
+        <div className={css.Header}>
+          <div className={css.Kanban}>Kanban board</div>
+          <div className={css.linkheading}>
+            <Link className={css.links} href={"https://github.com/Agastya221/Jira_clone_in_next_js"}>
+              <i className={css.icon}><GitHubIcon fontSize='small' /></i>
+              <p className={css.p}>Github Repo</p>
+            </Link>
+          </div>
         </div>
-      </div>
-      <DragDropContext>
-        <div className={css.dndcontiner}>
-          <div className={css.flexcontiner}>
-            <div className={css.heading}>
-             backlog 2
-            </div>
-          <div className={css.dragcontainer}>
-            <div className={css.dragAblecontainer}>
-              <p className={css.p}>
-              Try leaving a comment on this issue.
-              </p>
-
-              <div className={css.bottomtags}>
-              <i className={css.tick}><CheckBoxIcon/></i>
-              </div> 
-            </div>
-            </div>
-            
-            
+        <div className={css.container} >
+          <div className={css.inputContainer}>
+            <i className={css.search}><FiSearch color='black' /></i>
+            <input className={css.input} type="text" />
           </div>
-          <div className={css.flexcontiner}>
-            <div className={css.heading}>
-            Selected for development
-            </div>
-            <div className={css.dragcontainer}>
-            <div className={css.dragAblecontainer}>
-              <p className={css.p}>
-              Try leaving a comment on this issue.
-              </p>
-
-              <div className={css.bottomtags}>
-              <i className={css.tick}><CheckBoxIcon/></i>
-              </div> 
-            </div>
-            </div>
-            
-          </div>
-          <div className={css.flexcontiner}>
-            <div className={css.heading}>
-            In progress 2
-            </div>
-            <div className={css.dragcontainer}>
-            <div className={css.dragAblecontainer}>
-              <p className={css.p}>
-              Try leaving a comment on this issue.
-              </p>
-
-              <div className={css.bottomtags}>
-              <i className={css.tick}><CheckBoxIcon/></i>
-              </div> 
-            </div>
-            </div>
-          </div>
-          <div className={css.flexcontiner}>
-            <div className={css.heading}>
-              Done 2
-            </div>
-            <div className={css.dragcontainer}>
-            <div className={css.dragAblecontainer}>
-              <p className={css.p}>
-              Try leaving a comment on this issue.
-              </p>
-
-              <div className={css.bottomtags}>
-              <i className={css.tick}><CheckBoxIcon/></i>
-              </div> 
-            </div>
-            </div>
-          </div>
-
-
-
         </div>
-      </DragDropContext>
-    </Container>
-  </>
+        <div className={css.wholediv}>
+        {isBrowser ? <DragDropContext > 
+          <Droppable droppableId='dndcontainer'>
+            {(provided) => (
+              <div className={css.dndcontainer}>
+                <div className={css.flexcontainer}>
+                  <div className={css.heading}>
+                    backlog 2
+                  </div>
+                  <div className={css.dragcontainer}  ref={provided.innerRef} {...provided.droppableProps}>
+                  {dataWithIndexes.map((item) => (
+                    <Draggable key={item.id} draggableId={item.id} index={item.index}>
+                      {(provided) => {
+                        console.log(provided.dragHandleProps);
+                        return(
+                        <div  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <div className={css.dragAblecontainer} >
+                            <p className={css.ptagofdarg}>
+                              {item.text}
+                            </p>
+                            <div className={css.bottomtags}>
+                              <i className={css.tick}><CheckBoxIcon /></i>
+                            </div>
+                          </div>
+                         
+                        </div>
+                      )}}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  </div>
+                </div>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <Droppable droppableId='dndcontainer1'>
+            {(provided) => (
+              <div className={css.dndcontainer}>
+                <div className={css.flexcontainer}>
+                  <div className={css.heading}>
+                    backlog 2
+                  </div>
+                  <div className={css.dragcontainer}  ref={provided.innerRef} {...provided.droppableProps}>
+                  {dataWithIndexes1.map((item) => (
+                    <Draggable key={item.id} draggableId={item.id} index={item.index}>
+                      {(provided) => {
+                        console.log(provided.dragHandleProps);
+                        return(
+                        <div  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <div className={css.dragAblecontainer} >
+                            <p className={css.ptagofdarg}>
+                              {item.text}
+                            </p>
+                            <div className={css.bottomtags}>
+                              <i className={css.tick}><CheckBoxIcon /></i>
+                            </div>
+                          </div>
+                        </div>
+                      )}}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  </div>
+                </div>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <Droppable droppableId='dndcontainer2'>
+            {(provided) => (
+              <div className={css.dndcontainer}>
+                <div className={css.flexcontainer}>
+                  <div className={css.heading}>
+                    backlog 2
+                  </div>
+                  <div className={css.dragcontainer}  ref={provided.innerRef} {...provided.droppableProps}>
+                  {dataWithIndexes2.map((item) => (
+                    <Draggable key={item.id} draggableId={item.id} index={item.index}>
+                      {(provided) => {
+                        console.log(provided.dragHandleProps);
+                        return(
+                        <div  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <div className={css.dragAblecontainer} >
+                            <p className={css.ptagofdarg}>
+                              {item.text}
+                            </p>
+                            <div className={css.bottomtags}>
+                              <i className={css.tick}><CheckBoxIcon /></i>
+                            </div>
+                          </div>
+                        </div>
+                      )}}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  </div>
+                </div>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+           </DragDropContext> : null}
+           </div>
+      </Container>
+    </>
   )
 }
 
-export default mainContent
+
+export default MainContent;
